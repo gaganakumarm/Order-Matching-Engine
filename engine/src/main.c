129f95e0f71ee367eabadecc16084f90b71a9731
Gagana K <gaganakumarm@gmail.com>
2026-06-21 22:30:05 +0530

    Updated full CSV to matching workflow

--- engine/src/main.c
+++ engine/src/main.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 
 #include "csv_loader.h"
+#include "matching.h"
 #include "order_book.h"
+#include "trade.h"
 
 int main(void) {
 	OrderBook book;
 	int orders_loaded;
 
 	initOrderBook(&book);
+	initializeTradeHistory();
 	orders_loaded = loadOrdersFromCSV("data/orders.csv", &book);
-	if (orders_loaded == 0) {
-		orders_loaded = loadOrdersFromCSV("engine/data/orders.csv", &book);
-	}
 
 	printf("Orders Loaded: %d\n", orders_loaded);
-	printf("\n");
+	printf("\nBEFORE MATCHING\n");
+	printOrderBook(&book);
+
+	processMatches(&book);
+
+	printf("\nTrades Executed\n");
+	printTradeHistory();
+
+	printf("\nAFTER MATCHING\n");
 	printOrderBook(&book);
 
 	return 0;
